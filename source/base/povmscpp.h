670c21d4e033d0f7054dd2d2fcea499112596fcf
LeForgeron <leforgeron@free.fr>
2016-11-13 18:51:36 +0100

    previous commit makes the linker not happy
    --HG--
    branch : Hg-povray

--- source/base/povmscpp.h
+++ source/base/povmscpp.h
@@ -293,13 +293,13 @@ class POVMS_MessageReceiver
 		{
 			public:
 				virtual void Call(POVMS_Message&, POVMS_Message&, int) = 0;
-                virtual ~HandlerOO() = 0;
+                virtual ~HandlerOO(){}
 		};
 		class Handler
 		{
 			public:
 				virtual void Call(POVMSObjectPtr, POVMSObjectPtr, int) = 0;
-                virtual ~Handler() = 0;
+                virtual ~Handler(){};
 		};
 	protected:
 		template<class T> class MemberHandlerOO : public HandlerOO
